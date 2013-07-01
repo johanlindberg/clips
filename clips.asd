774e1c20457201ba43ea73fc9511627c30517abb
Johan Lindberg <johan@pulp.se>
2013-07-01 22:46:13 +0200

    Added a first step of the implementation for assert.
    The implementation as it is right now returns the defstruct that
    is created as an implied deftemplate. I suspect that's the 'right'
    behaviour. Whatever that means. Anyway. I'll add the code for the
    custom printout next.

--- clips.asd
+++ clips.asd
@@ -11,5 +11,4 @@
   :author "Johan Lindberg (johan@pulp.se)"
   :licence "Public Domain"
   :components ((:file "clips")
-	       (:file "fact-functions" :depends-on ("clips"))
 	       (:file "commands" :depends-on ("clips"))))
--- clips.lisp
+++ clips.lisp
@@ -3,6 +3,7 @@
 
 (defpackage :clips
   (:use :common-lisp)
+  (:shadow :assert)
   (:export :TRUE
 	   :FALSE
 
@@ -11,9 +12,42 @@
 	   :retract
 
 	   ; Commands
-	   :batch)
-  (:shadow :assert))
+	   :batch))
 (in-package :clips)
 
 (defconstant TRUE 'TRUE)
 (defconstant FALSE 'FALSE)
+
+(defvar *working-memory* (make-hash-table))
+(defvar *fact-index* 0)
+
+(defmacro %make-implied-deftemplate (name multislot)
+  "%make-implied-deftemplate constructs a defstruct with one multislot (%multislot)"
+
+  `(progn
+     (defstruct ,name %multislot)
+     (,(intern (concatenate 'string "MAKE-" (string name))) :%multislot ',multislot)))
+
+(defmacro assert (&rest rhs-patterns)
+  "The assert action allows the user to add a fact to the fact‑list.
+
+   Multiple facts may be asserted with each call. If the facts item is being
+   watched  (see section 13.2), then an informational message will be printed
+   each time a fact is asserted.
+
+   Syntax:
+     (assert <RHS-pattern>+)
+
+   Doctests:
+   >> (clips:assert (color red))
+   <Fact-0>
+   "
+  (let ((result '()))
+    (mapcar
+     #'(lambda (rhs-pattern)
+	 (setf result
+	       (append `(setf (gethash ,(incf *fact-index*) *working-memory*)
+			      (%make-implied-deftemplate ,(car rhs-pattern)
+							 ,(cdr rhs-pattern))))))
+     rhs-patterns)
+  result))
